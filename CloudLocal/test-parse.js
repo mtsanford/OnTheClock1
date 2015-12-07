337877a808f45f91fc09d08a63512e7a10cbbcb4
Mark Sanford <marktsanford@gmail.com>
2015-12-06 18:52:11 -0800

    WS summarizer candidate for parse cloud

--- CloudLocal/test-parse.js
+++ CloudLocal/test-parse.js
@@ -1,84 +1,127 @@
 
 $(function() {
 
-  //Parse.initialize("7vGvRyqr3tABuCPnHFHSMu7PlhJKy6F2YF7aL2Wr", "I0w0p4lAZhzZNQdkFJic2qNQsl8xoipuACo89QIo");
-  Parse.initialize("Mj76sGeuIdpclaGit0TEgWzqwMhHPJyjBFXRF7ml", "wCcNvtvZoPeCnWxdqpcj0SgWgJj6jxiJhwHlioe4");
+Parse.initialize("Mj76sGeuIdpclaGit0TEgWzqwMhHPJyjBFXRF7ml", "wCcNvtvZoPeCnWxdqpcj0SgWgJj6jxiJhwHlioe4");
 
-  var WorkSession = Parse.Object.extend("WorkSession");
-  var Activity = Parse.Object.extend("Activity");
+var WorkSession = Parse.Object.extend("WorkSession");
+var Activity = Parse.Object.extend("Activity");
   
-  if (!Parse.User.current()) {
-	  Parse.User.logIn("testset@marksanford.me", "mark", {
-	    success: function(user) {
+if (!Parse.User.current()) {
+	Parse.User.logIn("testset@marksanford.me", "mark", {
+		success: function(user) {
 			log(user.get("username") + " logged in");
-			testFetchWorkSessions();
+			runTests();
 		},
-	    error: function(user, error) {
+		error: function(user, error) {
 			log("error logging in... " + error.message)
-	    }
-	  });
-  }
-  else {
+		}
+	});
+}
+else {
 	log(Parse.User.current().get("username") + " already logged in");
-	testFetchWorkSessions();  	
-  }
+	runTests();  	
+}
   
-  $("#test-moment").click(function() {
-	  var query = new Parse.Cloud.run("testMoment", {}, {}).then(
-		  function(result) {
-			  log(result);
-		  },
-		  function(error) {
-			  log(error);
-		  }
-	  );
-  });
+function runTests() {
+	var user = Parse.User.current();
+	var unit = 'day';
+	var onOrBeforeDate = moment.utc('2015-11-09 07:00:00').toDate();
+	var locale = 'en';
+	var timeZone = 'America/Denver';
+	summarizeWorkSessions(user, unit, 3, onOrBeforeDate, locale, timeZone).then({
+		function(result) {
+			log('ok');
+			console.log(result);
+		},
+		function(error) {
+			log('huh?');
+			log(error.message);
+		}
+	});
+}
   
-  /*
-  	user			PFUser object
-  	unit			'day', 'week', or 'month'
-    onOrBeforeDate	Date object
-  	afterDate		Date object
-    locale			String (locale code used for determining first day of week - e.g. Sun or Mon)
-    timeZone		String
-  */
-function summarizeWorkSessions(user, unit, onOrBeforeDate, afterDate, locale, timeZone) {
-	var time, unitTimeString, activityName, duration, i, 
+$("#test-moment").click(function() {
+	var query = new Parse.Cloud.run("testMoment", {}, {}).then(
+		function(result) {
+			log(result);
+		},
+		function(error) {
+			log(error.message);
+		}
+	);
+});
+
+ 
+/*
+	user			PFUser object
+	unit			'day', 'week', or 'month'
+	howMany			how many units to summarize
+	onOrBeforeDate	Date object.   *Most recent* unit. 
+	locale			String (locale code used for determining first day of week - e.g. Sun or Mon)
+	timeZone		String
+
+	NOTE: The first unit of the returned results will be the unit onOrBeforeDate falls in.  So
+ 	if the unit is 'month', and onOrBeforeDate is 2015-03-15, then the first unit will be
+    2015-03-01 through 2015-03-31, with the index of 2015-03-01T00:00:00 in the timeZone provided.
+
+*/
+function summarizeWorkSessions(user, unit, howMany, onOrBeforeDate, locale, timeZone) {
+	var m, b, onOrBeforeMoment, afterDate, activityName, duration, i, 
+	    // moment.js uses different unit strings for startOf() and add()... frigin' genius
+	    momentAddUnits = { 'day' : 'days', 'week' : 'weeks', 'month' : 'months' },
 	    promise = new Parse.Promise(),
 	    buckets = {},
 	    result = [];
 
-	if (moment(onOrBeforeDate).isBefore(moment(afterDate))) {
-		return Parse.Promise.error("onOrBeforeDate is before afterDate");
+	if (momentAddUnits[unit] == undefined) {
+		return Parse.Promise.error("bad unit");
 	}
 	
+	onOrBeforeMoment = moment(onOrBeforeDate).tz(timeZone).locale(locale).startOf(unit);
 	
-	fetchWorkSessions(user, onOrBeforeDate, afterDate).then(
+	// Create buckets { '32423523523' : { 'activityname' : 360.00 }, ... }
+	m = onOrBeforeMoment.clone();
+	for (i=0; i<howMany; i++) {
+		buckets[m.valueOf()] = {}		
+		m.subtract(1, momentAddUnits[unit]);
+	}
+	
+	var keys = Object.keys(buckets);
+	log(keys.length);
+	for (i=0; i<keys.length; i++) {	
+		var timestamp = parseInt(keys[i]);
+		log(timestamp + " " + moment(timestamp).tz(timeZone).locale(locale).format());
+	}
+	
+	//return Parse.Promise.as("done");
+	
+	var beforeMoment = onOrBeforeMoment.clone().add(1, momentAddUnits[unit]);
+	var afterMoment = beforeMoment.clone().subtract(howMany, momentAddUnits[unit]);
+	console.log("afterMoment " + afterMoment.format());
+	console.log("beforeMoment " + beforeMoment.format());
+	fetchWorkSessions(user, afterMoment.toDate(), beforeMoment.toDate()).then(
 		function(workSessions) {
 			for (i=0; i<workSessions.length; i++) {
-				time = moment(workSessions[i].get('start'));
-				time.tz(timeZone);
-				time.locale(locale);
-				
-				// Need to hash as string value of unitTime, since javascript
-				// only allows string keys for hash dictionaries
-				unitTimeString = unitTime.startOf(unit).valueOf().toString();
+				m = moment(workSessions[i].get('start')).tz(timeZone).locale(locale).startOf(unit);
+				b = m.valueOf().toString();
+				log(m.format() + "looking for... " + b);
 				activityName = workSessions[i].get('activity').get('name');
 				duration = workSessions[i].get('duration');
-				if (buckets[unitTimeString] == undefined) { buckets[unitTimeString] = {} };
-				if (buckets[unitTimeString][activityName] == undefined) {
-					buckets[unitTimeString][activityName] = duration;
+				if (buckets[b] == undefined) { var s = "BAD bucket! " + b;  log(s); promise.reject(s); }
+				if (buckets[b][activityName] == undefined) {
+					buckets[b][activityName] = duration;
 				}
 				else {
-					buckets[unitTimeString][activityName] += duration;
+					buckets[b][activityName] += duration;
 				}
 			}
+			promise.resolve(buckets);
 		},
 		function(error) {
 			promise.reject(error);		  	
 		}
 	);
-	
+	log("asdfad");
 	return promise;
 }
   
@@ -93,7 +136,7 @@ function summarizeWorkSessions(user, unit, onOrBeforeDate, afterDate, locale, ti
   	returns Parse.Promise object
   
   */
-  function fetchWorkSessions(user, onOrBeforeDate, afterDate) {
+  function fetchWorkSessions(user, onOrAfterDate, beforeDate) {
 	  var itemsPerFetch = 500;
 	  var promise = new Parse.Promise();
 	  
@@ -101,18 +144,18 @@ function summarizeWorkSessions(user, unit, onOrBeforeDate, afterDate, locale, ti
 	  wsQuery.equalTo("user", user);
 	  wsQuery.include("activity");
 	  wsQuery.limit(itemsPerFetch);
-	  wsQuery.lessThanOrEqualTo("start", onOrBeforeDate);
-	  if (afterDate) { wsQuery.greaterThan("start", afterDate); }
-	  wsQuery.addDescending("start");
+	  if (onOrAfterDate) wsQuery.greaterThanOrEqualTo("start", onOrAfterDate);
+	  if (beforeDate) { wsQuery.lessThan("start", beforeDate); }
+	  wsQuery.addAscending("start");
 	  
 	  wsQuery.find().then(
 		  function(workSessions) {
 			  // if the query got the maximum amount of items, then query again
 			  // to see if there are more
 			  if (workSessions.length == itemsPerFetch) {
-				  var nextStartDate = new Date(workSessions[itemsPerFetch-1].get("start").getTime() - 1);
+				  var nextOnOrAfterDate = new Date(workSessions[itemsPerFetch-1].get("start").getTime() + 1);
 				  log("nextStartDate: " + nextStartDate);
-				  return fetchWorkSessions(user, nextStartDate, afterDate).then(
+				  return fetchWorkSessions(user, nextOnOrAfterDate, beforeDate).then(
 					  function(tailWorkSessions) {
 						  promise.resolve(workSessions.concat(tailWorkSessions));
 					  }
