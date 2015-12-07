40244d22a4df478ee9ec2302598f150424a602d6
Mark Sanford <marktsanford@gmail.com>
2015-12-07 06:38:58 -0800

    cleanup

--- CloudLocal/test-parse.js
+++ CloudLocal/test-parse.js
@@ -24,11 +24,11 @@ else {
   
 function runTests() {
 	var user = Parse.User.current();
-	var unit = 'day';
+	var unit = 'week';
 	var onOrBeforeDate = moment.utc('2015-11-09 07:00:00').toDate();
-	var locale = 'en';
-	var timeZone = 'America/Denver';
-	summarizeWorkSessions(user, unit, 3, onOrBeforeDate, locale, timeZone).then({
+	var locale = 'fr';
+	var timeZone = 'Europe/Paris';
+	summarizeWorkSessions(user, unit, 2, onOrBeforeDate, locale, timeZone).then(
 		function(result) {
 			log('ok');
 			console.log(result);
@@ -37,7 +37,7 @@ function runTests() {
 			log('huh?');
 			log(error.message);
 		}
-	});
+	);
 }
   
 $("#test-moment").click(function() {
@@ -56,18 +56,18 @@ $("#test-moment").click(function() {
 	user			PFUser object
 	unit			'day', 'week', or 'month'
 	howMany			how many units to summarize
-	onOrBeforeDate	Date object.   *Most recent* unit. 
-	locale			String (locale code used for determining first day of week - e.g. Sun or Mon)
+	firstUnitDate	Date object.   *Most recent* unit. 
+	locale			String (locale code.  Used for determining first day of week - e.g. Sun or Mon)
 	timeZone		String
 
-	NOTE: The first unit of the returned results will be the unit onOrBeforeDate falls in.  So
- 	if the unit is 'month', and onOrBeforeDate is 2015-03-15, then the first unit will be
+	NOTE: The first unit of the returned results will be the unit that firstUnitDate falls in.  So
+ 	if the unit is 'month', and firstUnitDate is 2015-03-15T13:45:00, then the first unit will be
     2015-03-01 through 2015-03-31, with the index of 2015-03-01T00:00:00 in the timeZone provided.
 
 */
-function summarizeWorkSessions(user, unit, howMany, onOrBeforeDate, locale, timeZone) {
-	var m, b, onOrBeforeMoment, afterDate, activityName, duration, i, 
-	    // moment.js uses different unit strings for startOf() and add()... frigin' genius
+function summarizeWorkSessions(user, unit, howMany, firstUnitDate, locale, timeZone) {
+	var m, b, firstUnitMoment, afterDate, activityName, duration, i, 
+	    // moment.js uses different unit strings for startOf() and add()... frigin' genius!
 	    momentAddUnits = { 'day' : 'days', 'week' : 'weeks', 'month' : 'months' },
 	    promise = new Parse.Promise(),
 	    buckets = {},
@@ -77,42 +77,32 @@ function summarizeWorkSessions(user, unit, howMany, onOrBeforeDate, locale, time
 		return Parse.Promise.error("bad unit");
 	}
 	
-	onOrBeforeMoment = moment(onOrBeforeDate).tz(timeZone).locale(locale).startOf(unit);
+	firstUnitMoment = moment(firstUnitDate).tz(timeZone).locale(locale).startOf(unit);
 	
-	// Create buckets { '32423523523' : { 'activityname' : 360.00 }, ... }
-	m = onOrBeforeMoment.clone();
+	// Create buckets { '32423523523' : { 'activityname1' : 360.00, ... }, ... }
+	m = firstUnitMoment.clone();
 	for (i=0; i<howMany; i++) {
 		buckets[m.valueOf()] = {}		
 		m.subtract(1, momentAddUnits[unit]);
 	}
 	
-	var keys = Object.keys(buckets);
-	log(keys.length);
-	for (i=0; i<keys.length; i++) {	
-		var timestamp = parseInt(keys[i]);
-		log(timestamp + " " + moment(timestamp).tz(timeZone).locale(locale).format());
-	}
-	
-	//return Parse.Promise.as("done");
-	
-	var beforeMoment = onOrBeforeMoment.clone().add(1, momentAddUnits[unit]);
-	var afterMoment = beforeMoment.clone().subtract(howMany, momentAddUnits[unit]);
-	console.log("afterMoment " + afterMoment.format());
-	console.log("beforeMoment " + beforeMoment.format());
-	fetchWorkSessions(user, afterMoment.toDate(), beforeMoment.toDate()).then(
+	// Need to add one unit, since firstUnitMoment is the START of the unit 
+	var maxMoment = firstUnitMoment.clone().add(1, momentAddUnits[unit]);
+	var minMoment = maxMoment.clone().subtract(howMany, momentAddUnits[unit]);
+	fetchWorkSessions(user, minMoment.toDate(), maxMoment.toDate()).then(
 		function(workSessions) {
 			for (i=0; i<workSessions.length; i++) {
-				m = moment(workSessions[i].get('start')).tz(timeZone).locale(locale).startOf(unit);
-				b = m.valueOf().toString();
-				log(m.format() + "looking for... " + b);
+				b = moment(workSessions[i].get('start')).tz(timeZone).locale(locale).startOf(unit).valueOf().toString();
 				activityName = workSessions[i].get('activity').get('name');
 				duration = workSessions[i].get('duration');
-				if (buckets[b] == undefined) { var s = "BAD bucket! " + b;  log(s); promise.reject(s); }
-				if (buckets[b][activityName] == undefined) {
-					buckets[b][activityName] = duration;
-				}
+				if (buckets[b] == undefined) { var s = "BAD bucket: " + b; log(s); promise.reject(s); }
 				else {
-					buckets[b][activityName] += duration;
+					if (buckets[b][activityName] == undefined) {
+						buckets[b][activityName] = duration;
+					}
+					else {
+						buckets[b][activityName] += duration;
+					}
 				}
 			}
 			promise.resolve(buckets);
@@ -121,17 +111,12 @@ function summarizeWorkSessions(user, unit, howMany, onOrBeforeDate, locale, time
 			promise.reject(error);		  	
 		}
 	);
-	log("asdfad");
 	return promise;
 }
   
   /*
-  	Fetch all WorkSessions that happened on or BEFORE onOrBeforeDate, but
-    after afterDate.   i.e.  afterDate > WS.start <= onOrBeforeDate
+  	Fetch all WorkSessions that happened onOrAfterDate >= WS.start > beforeDate
     Also loads activities
-  
-    IMPORTANT: afterDate is BEFORE onOrBeforeDate
-    afterDate is optional
   
   	returns Parse.Promise object
   
@@ -150,11 +135,9 @@ function summarizeWorkSessions(user, unit, howMany, onOrBeforeDate, locale, time
 	  
 	  wsQuery.find().then(
 		  function(workSessions) {
-			  // if the query got the maximum amount of items, then query again
-			  // to see if there are more
+			  // if the query got the maximum amount of items, then query again for more
 			  if (workSessions.length == itemsPerFetch) {
 				  var nextOnOrAfterDate = new Date(workSessions[itemsPerFetch-1].get("start").getTime() + 1);
-				  log("nextStartDate: " + nextStartDate);
 				  return fetchWorkSessions(user, nextOnOrAfterDate, beforeDate).then(
 					  function(tailWorkSessions) {
 						  promise.resolve(workSessions.concat(tailWorkSessions));
@@ -162,7 +145,6 @@ function summarizeWorkSessions(user, unit, howMany, onOrBeforeDate, locale, time
 				  )
 			  }
 			  else {
-				  log("query fetched " + workSessions.length)
 				  promise.resolve(workSessions);
 			  }
 		  },
